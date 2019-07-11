f1b76f37ecc5ddcd5fef37c8e372cb2c1bf817c0
Erich Grunewald <erich.grunewald@futurice.com>
2019-07-11 15:33:05 +0200

    Add filters for release date and catalogue number formatting

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,17 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addFilter("paddedCatalogNumber", number => {
+        if (number <= 999) {
+            number = ("000" + number).slice(-3);
+        }
+        return number;
+    });
+
+    eleventyConfig.addFilter("readableDate", dateObj => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('d MMMM y');
+    });
+
     return {
         templateFormats: [
             "md",
