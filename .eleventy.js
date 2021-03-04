d20ee1bc7d570a2d06a667dce3ae9fc592e029d1
Erich Grunewald <erichgrunewald@gmail.com>
2021-03-04 16:42:04 +0100

    Improve wording, fix formatting

--- .eleventy.js
+++ .eleventy.js
@@ -1,29 +1,22 @@
 const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addFilter("paddedCatalogNumber", number => {
-        if (number <= 999) {
-            number = ("000" + number).slice(-3);
-        }
-        return number;
-    });
+  eleventyConfig.addFilter("paddedCatalogNumber", (number) => {
+    if (number <= 999) {
+      number = ("000" + number).slice(-3);
+    }
+    return number;
+  });
 
-    eleventyConfig.addFilter("readableDate", dateObj => {
-        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('d MMMM y');
-    });
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("d MMMM y");
+  });
 
-    return {
-        templateFormats: [
-            "md",
-            "njk",
-            "html",
-            "css",
-            "png",
-            "jpg",
-        ],
-        passthroughFileCopy: true,
-        markdownTemplateEngine: "liquid",
-        htmlTemplateEngine: "njk",
-        dataTemplateEngine: "njk",
-    };
+  return {
+    templateFormats: ["md", "njk", "html", "css", "png", "jpg", "jpeg"],
+    passthroughFileCopy: true,
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  };
 };
